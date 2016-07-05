3670b76908c07b9a0a742d21505cb56870a687d8
lsprite <lsprite14@gmail.com>
2016-07-05 16:27:21 +0800

    修改

--- app/src/main/java/com/dodola/rocoosample/MainActivity.java
+++ app/src/main/java/com/dodola/rocoosample/MainActivity.java
@@ -7,9 +7,13 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
+import android.view.View;
 import android.widget.TextView;
 
 import com.dodola.rocoosample.service.NuwaDownloadService;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 
 public class MainActivity extends Activity {
@@ -18,8 +22,16 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("++++++++" + getCurrentRuntimeValue());
         TextView textView = (TextView) findViewById(R.id.textView);
         textView.setText(new HelloHack().showHello());
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
         //开启下载补丁服务
         if (false) {
             Intent service = new Intent(this, NuwaDownloadService.class);
@@ -27,6 +39,7 @@ public class MainActivity extends Activity {
             startService(service);
         }
     }
+
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
@@ -36,4 +49,49 @@ public class MainActivity extends Activity {
         }
         return false;
     }
+
+    //
+    private static final String SELECT_RUNTIME_PROPERTY = "persist.sys.dalvik.vm.lib";
+    private static final String LIB_DALVIK = "libdvm.so";
+    private static final String LIB_ART = "libart.so";
+    private static final String LIB_ART_D = "libartd.so";
+
+    //判断是dvm模式还是art模式
+    private CharSequence getCurrentRuntimeValue() {
+        try {
+            Class<?> systemProperties = Class.forName("android.os.SystemProperties");
+            try {
+                Method get = systemProperties.getMethod("get",
+                        String.class, String.class);
+                if (get == null) {
+                    return "WTF?!";
+                }
+                try {
+                    final String value = (String) get.invoke(
+                            systemProperties, SELECT_RUNTIME_PROPERTY,
+                        /* Assuming default is */"Dalvik");
+                    if (LIB_DALVIK.equals(value)) {
+                        return "Dalvik";
+                    } else if (LIB_ART.equals(value)) {
+                        return "ART";
+                    } else if (LIB_ART_D.equals(value)) {
+                        return "ART debug build";
+                    }
+
+                    return value;
+                } catch (IllegalAccessException e) {
+                    return "IllegalAccessException";
+                } catch (IllegalArgumentException e) {
+                    return "IllegalArgumentException";
+                } catch (InvocationTargetException e) {
+                    return "InvocationTargetException";
+                }
+            } catch (NoSuchMethodException e) {
+                return "SystemProperties.get(String key, String def) method is not found";
+            }
+        } catch (ClassNotFoundException e) {
+            return "SystemProperties class is not found";
+        }
+    }
+
 }
